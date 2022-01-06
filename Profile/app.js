388c6430aeb1d06231504f15307c1836e748241c
TejasASDF <tejasnambiar32@gmail.com>
2022-01-07 00:09:20 +0530

    Added and enabled CORS

--- Profile/app.js
+++ Profile/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = require('./Routes/api')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const mongoose = require('mongoose')
 
 // set up express app
 const app = express();
 const PORT = 4000;
+app.use(cors())
 app.use(bodyParser.json())
 //initializing routes
 app.use('/api',routes);
